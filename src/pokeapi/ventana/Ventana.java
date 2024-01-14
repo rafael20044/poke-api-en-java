f56b58f5ce0d5137f94a9c338e76eeb94958df35
Rafael <Rafaelenriquext@gmail.com>
2024-01-13 22:50:43 -0500

    Aca esta el codigo casi terminado

--- src/pokeapi/ventana/Ventana.java
+++ src/pokeapi/ventana/Ventana.java
@@ -1,5 +1,18 @@
 package pokeapi.ventana;
 
+import java.awt.Cursor;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import org.json.JSONObject;
+
 public class Ventana extends javax.swing.JFrame {
 
     public Ventana() {
@@ -14,6 +27,7 @@ public class Ventana extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         txtNombre = new javax.swing.JTextField();
         btnBuscar = new javax.swing.JButton();
+        lbMostrar = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -24,21 +38,31 @@ public class Ventana extends javax.swing.JFrame {
         txtNombre.setBorder(javax.swing.BorderFactory.createTitledBorder("Nombre"));
 
         btnBuscar.setText("Buscar");
+        btnBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBuscarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(218, 218, 218)
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(176, Short.MAX_VALUE)
                 .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(28, 28, 28)
                 .addComponent(btnBuscar)
                 .addGap(186, 186, 186))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(218, 218, 218)
+                        .addComponent(jLabel1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(210, 210, 210)
+                        .addComponent(lbMostrar, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -49,7 +73,9 @@ public class Ventana extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnBuscar))
-                .addContainerGap(259, Short.MAX_VALUE))
+                .addGap(52, 52, 52)
+                .addComponent(lbMostrar, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(129, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -66,11 +92,56 @@ public class Ventana extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
+        String nombre = txtNombre.getText();
+        try {
+            URL url = new URL("https://pokeapi.co/api/v2/pokemon/"+nombre);
+            HttpURLConnection co = (HttpURLConnection) url.openConnection();
+            co.setRequestMethod("GET");
+            co.connect();
+            int respuesta = co.getResponseCode();
+            if (respuesta == 200) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(co.getInputStream()));
+                StringBuilder builder = new StringBuilder();
+                String line;
+                setCursor(Cursor.WAIT_CURSOR);
+                while ((line = br.readLine()) != null ) {                    
+                    builder.append(line);
+                }
+                br.close();
+                
+                JSONObject json = new JSONObject(builder.toString());
+                String imagen = json.getJSONObject("sprites").getString("front_default");
+                mostar(imagen);
+                co.disconnect();
+            } else {
+                JOptionPane.showMessageDialog(this, "Error");
+                setCursor(Cursor.DEFAULT_CURSOR);
+            }
+        } catch (MalformedURLException ex) {
+            System.out.println(ex);
+            setCursor(Cursor.DEFAULT_CURSOR);
+        } catch (IOException ex) {
+            setCursor(Cursor.DEFAULT_CURSOR);
+            System.out.println(ex);
+        }
+    }//GEN-LAST:event_btnBuscarActionPerformed
+
+    private void mostar(String url){
+        try {
+            ImageIcon icon = new ImageIcon(new URL(url));
+            lbMostrar.setIcon(icon);
+            setCursor(Cursor.DEFAULT_CURSOR);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBuscar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lbMostrar;
     private javax.swing.JTextField txtNombre;
     // End of variables declaration//GEN-END:variables
 }
